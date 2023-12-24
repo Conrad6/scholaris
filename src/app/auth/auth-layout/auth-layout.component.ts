821ad7b031d986e29e29496a4309b6a1c3ec5736
Conrad Bekondo <philllittle302@gmail.com>
2023-12-24 07:14:05 +0100

    chore: stub

--- src/app/auth/auth-layout/auth-layout.component.ts
+++ src/app/auth/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthModule } from '@angular/fire/auth'
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthModule],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
