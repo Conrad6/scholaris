aa3d6e421f0a8fa5e8b3c03bc209bedc48ed0dd5
Conrad Bekondo <philllittle302@gmail.com>
2023-12-24 08:43:06 +0100

    chore: stub

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [CardModule, InputTextModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+  form = new FormGroup({
+    email: new FormControl<string>('', { validators: [Validators.required, Validators.email] })
+  })
 }
