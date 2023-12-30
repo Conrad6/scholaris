b06cb4cfdfadbba60fe55da6691adf74843a4a1a
Conrad Bekondo <philllittle302@gmail.com>
2023-12-30 20:17:59 +0100

    chore: stub

--- src/app/console/institutions-page/institutions-page.component.ts
+++ src/app/console/institutions-page/institutions-page.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { DataViewModule } from 'primeng/dataview';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-institutions-page',
   standalone: true,
-  imports: [],
+  imports: [
+    DataViewModule,
+    TooltipModule,
+    RouterLink,
+    ToolbarModule,
+    ButtonModule
+  ],
   templateUrl: './institutions-page.component.html',
   styleUrl: './institutions-page.component.scss'
 })
